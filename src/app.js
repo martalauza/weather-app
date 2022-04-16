86864d9b0a7a63b3b0e828d7205a073009d4cfca
Marta <martalauza@inbox.lv>
2022-04-16 18:50:12 +0300

    Created forecast template in JS

--- src/app.js
+++ src/app.js
@@ -21,7 +21,34 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
+  let forecastHTML = `<div class ="row">`;
+  let days = ["Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+     <div class="col-2">
+       <div class="weather-forecast-date">${day}</div>
+       <img
+         src="http://openweathermap.org/img/wn/04d@2x.png"
+         alt=""
+         width="42px"
+       />
+       <div class="weather-forecast-temperatures">
+         <span class="weather-forecast-temperature-max"> 18°</span>
+         <span class="weather-forecast-temperature-min"> 12° </span>
+       </div>
+     </div>
+  
+   `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastElement);
+}
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
@@ -82,3 +109,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("New York");
+displayForecast();
